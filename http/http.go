125c84b9e091a7a1b33a07577e7fd628170d5a13
Sprof22 <richmondelaigwu@gmail.com>
2023-08-22 12:33:56 +0100

    inputting into endpoint

--- http/http.go
+++ http/http.go
@@ -1,18 +1,43 @@
-// http.go
 package http
 
 import (
 	"fmt"
 	"net/http"
+	"sync"
+	"time"
 )
 
-func HelloWorldHandler(w http.ResponseWriter, r *http.Request, message *string) {
-	fmt.Fprintln(w, *message)
+var message = ""
+var mu sync.Mutex
+
+func HelloWorldHandler(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	defer mu.Unlock()
+	message := getMessage() //get the message
+	fmt.Fprintln(w, message)
 }
 
-func StartServer(message *string) {
-	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		HelloWorldHandler(w, r, message)
-	})
+func getMessage() string {
+	return message
+}
+
+func addHello() {
+	time.Sleep(5 * time.Second)
+	mu.Lock()
+	defer mu.Unlock()
+	message += "Hello"
+}
+
+func addWorld() {
+	time.Sleep(10 * time.Second)
+	mu.Lock()
+	defer mu.Unlock()
+	message += " world!"
+}
+
+func StartServer() {
+	http.HandleFunc("/hello", HelloWorldHandler)
+	go addHello()
+	go addWorld()
 	http.ListenAndServe(":8080", nil)
 }
